6d8a0eb4f5d04b690e56fe8d6c5de26f5225229e
shubhamCredenc <shubham.jha@credenc.com>
2022-01-17 18:39:05 +0530

    responsive

--- src/components/elementalComponents/smallTable/SmallTable.js
+++ src/components/elementalComponents/smallTable/SmallTable.js
@@ -5,7 +5,7 @@ export default function SmallTable({ list, handleCheckBox, dependent=false, sele
     return (
         <div className='small-table'>
             <div className='thead'>Installments</div>
-            <div className='tbody' style={{maxHeight: dependent && '23rem'}}>
+            <div className='tbody' style={{height: dependent && '35vh'}}>
                 {list.map((installment, i) => (
                     <CollapsibleInstallment installment={installment} index={i} handleCheckBox={handleCheckBox} key={i}/>
                 ))}
