6cf91d8d7171c8b43bc62cc6ff007cebe762b890
Apple <manish.soni@credenc.com>
2023-06-07 17:11:35 +0530

    sentry updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/login/Login';
 import Home from './components/home/Home';
@@ -15,22 +15,6 @@ import Signup from './components/signup/Signup';
 import AdhocSuccess from './components/adhocSuccess/AdhocSuccess';
 
 function App() {
-
-  Sentry.init({
-    dsn: "https://8eb2f6c7062c4a1497f8dc2f79e27810@o4505310984404992.ingest.sentry.io/4505312167723008",
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-      }),
-      new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
 
   return(
     <>
