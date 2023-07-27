ba7d3c680e0f3fac69b665ac4b2b826170199ea9
Apple <manish.soni@credenc.com>
2023-07-27 12:07:03 +0530

    link expired

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -18,6 +18,7 @@ const tenures = [
 
 export default function Signup() {
     const [applyForLoan,setApplyForLoan] = useState(false)
+    const [linkExpired, setLinkExpired] = useState(false);
     const [collegeInfo,setCollegeInfo] = useState({})
     const [tenure,setTenure] = useState(0)
     const [remark,setRemark] = useState('');
@@ -202,6 +203,10 @@ export default function Signup() {
         }
     },[nameChecked])
 
+    const handleLinkExpired=()=>{
+        setLinkExpired(true)
+    }
+
     return (
         <div className='signup'>
 
@@ -284,10 +289,20 @@ export default function Signup() {
                     openApplyForLoan={(data,collegeData,adhocData,amount)=>openApplyForLoan(data,collegeData,adhocData,amount)}
                     closeApplyForLoan={()=>closeApplyForLoan()}
                     fieldData={instituteDetails}
+                    handleLinkExpired={()=>handleLinkExpired()}
                 />
             </div>
                 
-          
+            {
+                linkExpired &&
+                <div className='under-construction-container'>
+                <div className='under-construction-overlay'></div>
+                    <div className='under-construction-content'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ff0000" viewBox="0 0 256 256"><path d="M128,28A100,100,0,1,0,228,128,100.11,100.11,0,0,0,128,28Zm92,100a91.67,91.67,0,0,1-24.21,62.13L65.87,60.21A92,92,0,0,1,220,128ZM36,128A91.67,91.67,0,0,1,60.21,65.87L190.13,195.79A92,92,0,0,1,36,128Z"></path></svg>
+                    <div className=''>This Link is Already Expired</div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
